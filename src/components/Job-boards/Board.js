b7888bfdc57d4613e8c7c057921d27cd9467c69d
Jonathan Woodard <jvw.226@gmail.com>
2022-12-09 14:32:17 -0600

    create board and dashboard components

--- src/components/Job-boards/Board.js
+++ src/components/Job-boards/Board.js
@@ -1,27 +1,69 @@
 import { useState, useEffect } from "react";
-import { getAllBoardsForUser } from "../managers/BoardManager";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteBoard, getAllBoardsForUser, getSingleBoardForUser } from "../managers/BoardManager";
 
 export const BoardView = () => {
-const[boards, setBoards] = useState([])
+  const [board, setBoard] = useState({});
 
+  const { id } = useParams();
+  const navigate = useNavigate();
+  console.log(id)
 
-useEffect(() => {
-    getAllBoardsForUser()
-      .then((userBoards) => {
-        setBoards(userBoards);
-      });
+  useEffect(() => {
+    getSingleBoardForUser(id).then((userBoard) => {
+      setBoard(userBoard);
+    });
   }, []);
 
+const renderDeleteButton = (id) => {
+    return (
+      <>
+        <button
+          className="button is-small is-danger is-focused"
+          onClick={() => {
+            if (
+              window.confirm("Are you sure you want to delete this Board?")
+            ) {
+              deleteRequestForBoard(id);
+            }
+          }}
+        > Delete Board
+        </button>
+      </>
+    );
+  };
 
 
+  const deleteRequestForBoard = (id) => {
+    deleteBoard(id)
+      .then(() => {
+        getAllBoardsForUser();
+      })
+      .then(() => navigate("/"));
+  };
 
-    return<>
-    <h1>Dashboard</h1>
-    <div>
-        {
-            boards.map()
-        }
-    </div>
+  return (
+    <>
+      <h1>{board.title}</h1>
+      <button
+        onClick={() => {
+          navigate(`/boards/${id}/edit`);
+        }}
+      >
+        Edit Board
+      </button>
+      {renderDeleteButton(id)}
+      <h2>Priorities</h2>
+      <h2>Goal</h2>
+      <div>{board.goal}</div>
+      <h2>Requirements</h2>
+      <div>{board.requirements}</div>
+      <h3>Ready To Apply</h3>
+      <h3>Applied</h3>
+      <h3>Interviewed</h3>
+      <h3>Offered</h3>
+      <h3>Denied</h3>
+      <h3></h3>
     </>
-}
-
+  );
+};
