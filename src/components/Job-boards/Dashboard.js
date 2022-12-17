84e542336366c991458f01c579196c31ddb7fcd5
Jonathan Woodard <jvw.226@gmail.com>
2022-12-17 01:21:10 -0500

    updated dashboard

--- src/components/Job-boards/Dashboard.js
+++ src/components/Job-boards/Dashboard.js
@@ -14,38 +14,44 @@ useEffect(() => {
   }, []);
 
 
-    return<>
-    <h1>Dashboard</h1>
-    <div>
+    return (
+      <>
+        <h1>Dashboard</h1>
         <div>
+          <div>
             <button
-            onClick={() => {
+              onClick={() => {
                 navigate(`/createboard`);
-                }}>Create New Board</button>
-        </div>
-        {
-            boards.map(board =>
-                <>
-                <div key={`board--${board.id}`}>
-                <div>{board.title}</div>
+              }}
+            >
+              Create New Board
+            </button>
+          </div>
+          {boards.map((board) => (
+            <>
+              <div key={`board--${board.id}`}>
+                <h2>{board.title}</h2>
                 <button
-                onClick={() => {
-                navigate(`/boards/${board.id}`);
-                }}>
-                View Board
+                  onClick={() => {
+                    navigate(`/boards/${board.id}`);
+                  }}
+                >
+                  View Board
                 </button>
-                <div>{board.goal}</div>
-                <div>{board.requirements}</div>
+                <h3>Goal</h3>
+                <h4>{board.goal}</h4>
+                <h3>Requirements</h3>
+                <h4>{board.requirements}</h4>
                 <div>Total Applications</div>
                 <div>Total Interviews</div>
                 <div>Total Offers</div>
-                </div>
-                </>
-            )
-        }
-    <div>Upcoming Interviews</div>
-    <div>Upcoming Recruiting Calls</div>
-    <div>Upcoming Networking Calls</div>
-    </div>
-    </>
+              </div>
+            </>
+          ))}
+          <div>Upcoming Interviews</div>
+          <div>Upcoming Recruiting Calls</div>
+          <div>Upcoming Networking Calls</div>
+        </div>
+      </>
+    );
 }
