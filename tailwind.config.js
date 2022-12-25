49e02f8d7ed0e60e6ca9f6cc827be11235eab622
Jonathan Woodard <jvw.226@gmail.com>
2022-12-25 15:07:42 -0500

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        "seeker-blue": "#1d4ed8",
+        "seeker-gray": "#e2e8f0",
+        "home-blue": "#0b0b41",
+        "home-orange": "#8e4318",
+      },
+    },
   },
-  plugins: [require("flowbite/plugin")],
 };
