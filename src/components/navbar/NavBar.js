cdfbb1e23ba14ebf69162e653d92245ae2f62424
Jonathan Woodard <jvw.226@gmail.com>
2022-12-25 15:42:12 -0500

    updated NavBar for unauthorized view

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,91 +1,42 @@
-import { useRef } from "react"
-import { Link, useLocation, useNavigate } from "react-router-dom"
-
+import { useRef } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export const NavBar = ({ token, setToken }) => {
-  const navigate = useNavigate()
-  const navbar = useRef()
-  const hamburger = useRef()
-  const { pathname } = useLocation()
+  const navigate = useNavigate();
+  const navbar = useRef();
 
-  const showMobileNavbar = () => {
-    hamburger.current.classList.toggle('is-active')
-    navbar.current.classList.toggle('is-active')
-  }
-  // console.log(pathname)
-
+  const NavLogoText = "< JobSeeker />"
   const renderNav = () => {
-    if (pathname !== "/" || pathname !== "/login") {
       return (
         <>
-          <nav
-            className="navbar is-success mb-3"
-            role="navigation"
-            aria-label="main navigation"
-          >
-            <div className="navbar-brand">
+          <nav className="h-20 flex justify-between bg-gradient-to-r from-home-orange via-home-blue to-home-orange">
+            <div className="flex">
               <a className="navbar-item" href="/">
-                <h1 className="title is-4">Job Seeker</h1>
-              </a>
-
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a
-                role="button"
-                className="navbar-burger"
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbarBasicExample"
-                onClick={showMobileNavbar}
-                ref={hamburger}
-              >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
+                <h1 className="text-white text-2xl p-4 font-mono ">
+                  {NavLogoText}
+                </h1>
               </a>
             </div>
-
-            <div className="navbar-menu" ref={navbar}>
-              <div className="navbar-start">
-                    <Link to="/dashboard" className="navbar-item">
-                      Dashboard
-                    </Link>
-                    <Link to="/boards" className="navbar-item">
-                      My Boards
-                    </Link>
-                    <Link to="/myposts" className="navbar-item">
-                      Network
-                    </Link>
-                    <Link to="/categories" className="navbar-item">
-                      Interview Prep
-                    </Link>
-                    <Link to="/tags" className="navbar-item">
-                      Resources
-                    </Link>
-              </div>
-              <div className="navbar-end">
-                <div className="navbar-item">
-                  <div className="buttons">
-                      <button
-                        className="button is-outlined"
-                        onClick={() => {
-                          localStorage.removeItem("seeker_token");
-                          window.location.reload("/");
-                        }}
-                      >
-                        Logout
-                      </button>
-                  </div>
-                </div>
-              </div>
+            <div className="flex w-3/12 gap-4 " ref={navbar}>
+              <Link to="/about" className="text-white text-xl font-mono">
+                About
+              </Link>
+              <Link to="/features" className="text-white text-xl font-mono">
+                Features
+              </Link>
+              <Link to="/contact" className="text-white text-xl font-mono">
+                Contact 
+              </Link>
+              <Link to="/login" className="text-white text-xl font-mono">
+                Log in
+              </Link>
+              <Link to="/register" className="text-white text-xl font-mono  rounded-md h-fit p-2 bg-home-blue">
+                Sign Up Free
+              </Link>
             </div>
           </nav>
         </>
       );
-    } else {
-      return "";
-    }
-  }
-  return (
-    renderNav()
-  )
-}
+  };
+  return renderNav();
+};
