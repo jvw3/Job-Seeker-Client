60a4e873bf2401e290d780614424a71f18428f4a
Jonathan Woodard <jvw.226@gmail.com>
2022-12-25 15:12:30 -0500

    updated jobseeker.js file to handle seperate views for authorized and unauthorized views

--- src/JobSeeker.js
+++ src/JobSeeker.js
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { ApplicationViews } from "./components/views/ApplicationViews"
 import { NavBar } from "./components/navbar/NavBar"
+import { SidebarNav } from "./components/views/SideBar"
+import { UnauthorizedViews } from "./components/views/UnauthorizedViews"
 
 
 export const JobSeeker = () => {
@@ -12,10 +14,23 @@ export const JobSeeker = () => {
   }
 
 
-  return <>
-  <div>
-    <NavBar token={token} setToken={setToken} />
-    <ApplicationViews token={token} setToken={setToken} />
-  </div>
-  </>
+  return (
+    <>
+      <div className="grid-cols-1">
+        {token ? (
+          <>
+            <main className="flex">
+              <SidebarNav token={token} setToken={setToken} />
+              <ApplicationViews token={token} setToken={setToken} />
+            </main>
+          </>
+        ) : (
+          <>
+            <NavBar token={token} setToken={setToken} />
+            <UnauthorizedViews token={token} setToken={setToken} />
+          </>
+        )}
+      </div>
+    </>
+  );
 }
