180a54a71108b01c9e234f328125fb756d90afc5
Jonathan Woodard <jvw.226@gmail.com>
2022-12-27 13:37:21 -0500

    created functional table for network view

--- src/components/network/NetworkHome.js
+++ src/components/network/NetworkHome.js
@@ -20,82 +20,112 @@ export const NetworkHome = () => {
 
   return (
     <>
-      <h1 className="text-4xl">Network</h1>
-      <div>
+      <main className="w-full flex-col">
+        <h1 className="text-4xl">Network</h1>
         <div>
-          <button
-            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 mb-2"
-            onClick={() => {
-              navigate(`/createboard`);
-            }}
-          >
-            Add Contact
-          </button>
-        </div>
-        <table className="">
-          <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-              >
-                Name
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                Title
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                Email
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                Role
-              </th>
-              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200 bg-white">
-            {contacts.map((contact) => (
-              <>
-                <tr
-                  className="flex justify-evenly mt-6 border gap-x-4"
-                  key={`contact--${contact.id}`}
+          <div>
+            <button
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 mb-2"
+              onClick={() => {
+                navigate(`/createboard`);
+              }}
+            >
+              Add Contact
+            </button>
+          </div>
+          <table className="shadow-2xl border-pinkswirl">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                 >
-                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                    {contact.name}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    {contact.current_role}
-                  </td>
-                  <div className="">{contact.current_company}</div>
-                  <div className="">{contact.last_contact}</div>
-                  <div className="">{contact.number_of_contacts}</div>
-                  <div className="">{contact.connection_level}</div>
-                  <button
-                    onClick={() =>
-                      openLinkInNewTab(`https://${contact.linked_in}`)
-                    }
-                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-4  text-center mr-2 mb-2"
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Current Role
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Company
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Number Of Contacts
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Connection Level
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  LinkedIn
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Notes
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Edit
+                </th>
+                <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                  <span className="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {contacts.map((contact) => (
+                <>
+                  <tr
+                    className="flexjustify-evenly mt-6 border gap-x-4"
+                    key={`contact--${contact.id}`}
                   >
-                    <IconBrandLinkedin />
-                  </button>
-                  <div className="">{contact.notes}</div>
-                </tr>
-              </>
-            ))}
-          </tbody>
-        </table>
-      </div>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                      {contact.name}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {contact.current_role}
+                    </td>
+                    <td className="">{contact.current_company}</td>
+                    <td className="">{contact.number_of_contacts}</td>
+                    <td className="">{contact.connection_level}</td>
+                    <td>
+                      <button
+                        onClick={() =>
+                          openLinkInNewTab(`https://${contact.linked_in}`)
+                        }
+                        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-4  text-center mr-2 mb-2"
+                      >
+                        <IconBrandLinkedin />
+                      </button>
+                    </td>
+                    <td className="">{contact.notes}</td>
+                    <td>
+                      <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-4  text-center mr-2 mb-2"> Edit Contact</button>
+                    </td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </>
   );
 };
