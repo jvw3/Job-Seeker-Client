041e6c64618f4d782e7e0cea6bb661dfaba75a64
Jonathan Woodard <jvw.226@gmail.com>
2022-12-27 13:34:06 -0500

    updated login screen

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -29,43 +29,44 @@ export const Login = ({ setToken }) => {
   }
 
   return (
-    <main className="min-h-screen flex justify-center align-center bg-gradient-to-r from-home-orange via-home-blue to-home-orange">
-      <section className="self-center border p-10 rounded-md bg-white">
+    <main className="min-h-screen flex justify-center  bg-pinkswirl h-full">
+      <section className="self-center border p-10 rounded-md bg-white w-3/4">
         <form className="" onSubmit={handleLogin}>
-          <h1 className="title">Job Seeker</h1>
-          <p className="subtitle">Please sign in</p>
-
-          <div className="field">
-            <label className="label">Username</label>
-            <div className="control">
+          <h1 className="text-3xl text-center">Job Seeker</h1>
+          <fieldset className="flex-col ">
+            <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+              <label className="block text-xs font-medium text-gray-900">
+                Username
+              </label>
               <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
                 type="text"
                 ref={username}
               />
             </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Password</label>
-            <div className="control">
+            <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+              <label className="block text-xs font-medium text-gray-900">
+                Password
+              </label>
               <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
                 type="password"
                 ref={password}
               />
             </div>
-          </div>
-
+          </fieldset>
           <div className="field is-grouped">
             <div className="control">
-              <button className="button is-link" type="submit">
+              <button
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 mb-2 mt-4 w-40"
+                type="submit"
+              >
                 Submit
               </button>
             </div>
-            <div className="control">
-              <Link to="/register" className="button is-link is-light">
-                Cancel
+            <div>
+              <Link to="/" className="button is-link is-light">
+                Back Home
               </Link>
             </div>
           </div>
