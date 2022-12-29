a81cc44eae425af3cee7230df0873baa77f11126
Jonathan Woodard <jvw.226@gmail.com>
2022-12-29 15:36:31 -0600

    updated styling for DashBoard and Individual Job Details components

--- src/components/Job-boards/Dashboard.js
+++ src/components/Job-boards/Dashboard.js
@@ -41,7 +41,7 @@ export const Dashboard = () => {
             {returnTimeAdjustWelcomeText()} {currentUser.firstName}{" "}
           </h1>
         </div>
-        <div className="h-1/5 m-4 rounded-md bg-pinkswirl bg-cover backdrop-filter backdrop-blur-md">
+        <div className="h-1/5 m-4 rounded-md bg-pinkswirl bg-cover filter backdrop-blur-md">
           <button
             className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 mb-2"
             onClick={() => {
