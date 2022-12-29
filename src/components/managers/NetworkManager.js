07dc48eb85aa8106fd9a6d80eccecf1c530d217e
Jonathan Woodard <jvw.226@gmail.com>
2022-12-29 15:33:58 -0600

    added new fetch requests to get data from server for network related components

--- src/components/managers/NetworkManager.js
+++ src/components/managers/NetworkManager.js
@@ -8,3 +8,50 @@ export const getContactsForUser = () => {
     },
   }).then((res) => res.json());
 };
+
+export const createContact = (contact) => {
+  return fetch("http://localhost:8000/contacts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+    body: JSON.stringify(contact),
+  }).then((res) => res.json());
+};
+
+export const getSingleContact = (id) => {
+  return fetch(`http://localhost:8000/contacts/${id}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+  }).then((res) => res.json());
+};
+
+export const updateContact = (contact, contactId) => {
+  return fetch(`http://localhost:8000/contacts/${contactId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+    body: JSON.stringify(contact),
+  });
+};
+
+export const getAllContactsByNameSearch = (searched) => {
+  return fetch(`http://localhost:8000/contacts?name=${searched}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+  }).then((res) => res.json());
+};
+
+
+export const sendConnectionFilterRequest = (connectionValue) => {
+  return fetch(`http://localhost:8000/contacts?connection=${connectionValue}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+  }).then((res) => res.json());
+};
