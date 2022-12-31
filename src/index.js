59c7abbf23ad613762120ced1e3d954092fb14e4
Jonathan Woodard <jvw.226@gmail.com>
2022-12-31 16:14:06 -0600

    daisyui install

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import { BrowserRouter } from "react-router-dom"
-import { JobSeeker } from './JobSeeker'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { JobSeeker } from "./JobSeeker";
 
-const container = document.getElementById("root")
-const root = createRoot(container)
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-    <BrowserRouter>
-        <JobSeeker />
-    </BrowserRouter>
-)
+  <BrowserRouter>
+      <JobSeeker />
+  </BrowserRouter>
+);
