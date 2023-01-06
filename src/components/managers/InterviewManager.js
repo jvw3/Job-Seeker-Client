c3a45df3103270421f52e5474a253a9408471938
Jonathan Woodard <jvw.226@gmail.com>
2023-01-06 10:26:12 -0600

    updated managers

--- src/components/managers/InterviewManager.js
+++ src/components/managers/InterviewManager.js
@@ -11,6 +11,17 @@ export const createInterview = (interview) => {
   }).then((res) => res.json());
 };
 
+export const createCustomPrep = (prep) => {
+  return fetch("http://localhost:8000/custompreps", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+    body: JSON.stringify(prep),
+  }).then((res) => res.json());
+};
+
 export const getUpcomingInterviewsForUser = () => {
   return fetch(`http://localhost:8000/interviews?upcoming`, {
     headers: {
@@ -37,8 +48,26 @@ export const deleteInterview = (id) => {
   });
 };
 
+
+export const deleteCustomPrep = (id) => {
+  return fetch(`http://localhost:8000/custompreps/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+  });
+};
+
 export const getSingleInterview = (id) => {
   return fetch(`http://localhost:8000/interviews/${id}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
+    },
+  }).then((res) => res.json());
+}
+
+export const getSingleInterviewPrep = (id) => {
+  return fetch(`http://localhost:8000/interviewpreps/${id}`, {
     headers: {
       Authorization: `Token ${localStorage.getItem("seeker_token")}`,
     },
