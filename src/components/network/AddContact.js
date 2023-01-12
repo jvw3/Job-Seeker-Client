3276ca88cee36c0bbb2a758ee5ca3a16735308a8
Jonathan Woodard <jvw.226@gmail.com>
2023-01-12 09:26:14 -0600

    created add network form

--- src/components/network/AddContact.js
+++ src/components/network/AddContact.js
@@ -208,7 +208,7 @@ export const ContactForm = () => {
                     autoFocus
                     type="textarea"
                     className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-                    placeholder="What are your job requirements?"
+                    placeholder=""
                     value={contact.notes}
                     onChange={(evt) => {
                       const copy = { ...contact };
