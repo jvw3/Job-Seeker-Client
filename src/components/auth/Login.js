2bb39e86ccb3705f75303f7e9e8c582ce77adf53
Jonathan Woodard <jvw.226@gmail.com>
2023-01-12 09:24:13 -0600

    updated Styling for register and login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -3,9 +3,10 @@ import { Link, useNavigate } from "react-router-dom"
 import { LoginUser } from "../managers/AuthManager"
 import { IconArrowLeft} from "@tabler/icons"
 import SalyDeskImage from "../views/HompageImages/Saly-10.png";
+import SeekerLogoBlueZoom from "../views/HompageImages/SeekerLogoBlueZoom.svg";
 
 
-export const Login = ({ setToken, setUserId }) => {
+export const Login = ({ setToken, setUserId, setIsStaff }) => {
   const username = useRef()
   const password = useRef()
   const navigate = useNavigate()
@@ -22,6 +23,7 @@ export const Login = ({ setToken, setUserId }) => {
     LoginUser(user).then(res => {
       if ("valid" in res && res.valid) {
         setToken(res.token)
+        setIsStaff(res.isStaff)
         setUserId(res.userId)
         navigate("/dashboard")
       }
@@ -44,56 +46,54 @@ export const Login = ({ setToken, setUserId }) => {
         >
           <IconArrowLeft color="white" className="" />
         </button>
-        <div className="h-3/4 flex-col items-center">
-          <div className="w-full flex justify-center">
-          <h1 className="text-4xl text-white">{NavLogoText}</h1>
-          </div>
-          <div className="w-full h-full">
-          <section className="justify-self-center border p-10 rounded-md bg-white w-3/4 flex-col">
-            <form className="" onSubmit={handleLogin}>
-              <fieldset className="flex-col">
-                <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
-                  <label className="block text-xs font-medium text-gray-900">
-                    Username
-                  </label>
-                  <input
-                    className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-                    type="text"
-                    ref={username}
-                  />
+        <div className="h-full">
+          <div className="flex justify-center">
+            <section className="self-center border p-10 rounded-md bg-white w-3/4 flex-col h-1/2 mt-14">
+              <img className="w-1/2 mb-10 ml-32" src={SeekerLogoBlueZoom}></img>
+              <form className="" onSubmit={handleLogin}>
+                <fieldset className="flex-col space-y-2">
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      Username
+                    </label>
+                    <input
+                      className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                      type="text"
+                      ref={username}
+                    />
+                  </div>
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      Password
+                    </label>
+                    <input
+                      className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                      type="password"
+                      ref={password}
+                    />
+                  </div>
+                </fieldset>
+                <div className="field is-grouped">
+                  <div className="control">
+                    <button
+                      className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 mb-2 mt-4 w-40"
+                      type="submit"
+                    >
+                      Log in
+                    </button>
+                  </div>
                 </div>
-                <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
-                  <label className="block text-xs font-medium text-gray-900">
-                    Password
-                  </label>
-                  <input
-                    className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-                    type="password"
-                    ref={password}
-                  />
-                </div>
-              </fieldset>
-              <div className="field is-grouped">
-                <div className="control">
-                  <button
-                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 mb-2 mt-4 w-40"
-                    type="submit"
-                  >
-                    Log in
-                  </button>
-                </div>
-              </div>
-              {isUnsuccessful ? (
-                <p className="text-red-600">Username or password not valid</p>
-              ) : (
-                ""
-              )}
-            </form>
-          </section>
+                {isUnsuccessful ? (
+                  <p className="text-red-600">Username or password not valid</p>
+                ) : (
+                  ""
+                )}
+              </form>
+            </section>
           </div>
         </div>
       </div>
-      <section className="bg-home w-1/2">
+      <section className="bg-home w-1/2 flex justify-center">
         <img className="h-3/4" src={SalyDeskImage}></img>
       </section>
     </main>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,6 +2,9 @@ import { useRef } from "react"
 import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import { registerUser } from "../managers/AuthManager"
+import { IconArrowLeft } from "@tabler/icons";
+import SalyDeskImage from "../views/HompageImages/Saly-10.png";
+import SeekerLogoBlueZoom from "../views/HompageImages/SeekerLogoBlueZoom.svg";
 
 export const Register = ({setToken}) => {
   const firstName = useRef()
@@ -32,7 +35,6 @@ export const Register = ({setToken}) => {
       registerUser(newUser)
         .then(res => {
           if ("successful" in res && res.successful) {
-            setToken(res.token)
             navigate("/login")
           }
         })
@@ -42,101 +44,149 @@ export const Register = ({setToken}) => {
   }
 
   return (
-    <section className="columns is-centered">
-      <form className="column is-two-thirds" onSubmit={handleRegister}>
-        <h1 className="title">Rare Publishing</h1>
-        <p className="subtitle">Create an account</p>
-        <div className="field">
-          <label className="label">First Name</label>
-          <div className="control">
-            <input className="input" type="text" ref={firstName} />
-          </div>
-        </div>
+    <main className="min-h-screen flex justify-center  bg-pinkswirl h-full">
+      <div className="flex-col w-1/2 p-5">
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <IconArrowLeft color="white" className="" />
+        </button>
+        <div className="h-full">
+          <div className="flex justify-center">
+            <section className="self-center border p-5 rounded-md bg-white w-3/4 flex-col h-80">
+              <img className="w-1/6 ml-60" src={SeekerLogoBlueZoom}></img>
+              <form className="column is-two-thirds" onSubmit={handleRegister}>
+                <h1 className="title mb-2"></h1>
+                <p className="mb-2 text-seeker-blue text-2xl font-quicksand">
+                  Create an account
+                </p>
+                <fieldset className="space-y-2">
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      First Name
+                    </label>
+                    <div className="control">
+                      <input
+                        className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                        type="text"
+                        ref={firstName}
+                      />
+                    </div>
+                  </div>
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      Last Name
+                    </label>
+                    <div className="control">
+                      <input
+                        className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                        type="text"
+                        ref={lastName}
+                      />
+                    </div>
+                  </div>
 
-        <div className="field">
-          <label className="label">Last Name</label>
-          <div className="control">
-            <input className="input" type="text" ref={lastName} />
-          </div>
-        </div>
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      Username
+                    </label>
+                    <div className="control">
+                      <input
+                        className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                        type="text"
+                        ref={username}
+                      />
+                    </div>
+                  </div>
 
-        <div className="field">
-          <label className="label">Username</label>
-          <div className="control">
-            <input className="input" type="text" ref={username} />
-          </div>
-        </div>
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      Email
+                    </label>
+                    <div className="control">
+                      <input
+                        className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                        type="email"
+                        ref={email}
+                      />
+                    </div>
+                  </div>
+                </fieldset>
 
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control">
-            <input className="input" type="email" ref={email} />
-          </div>
-        </div>
+                <div className="field mt-3 mb-3">
+                  <label className="text-seeker-blue font-quicksand">
+                    Password
+                  </label>
+                  <div className="space-y-2">
+                    <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                      <p className="block text-xs font-medium text-gray-900">
+                        <input
+                          className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                          type="password"
+                          placeholder="Password"
+                          ref={password}
+                        />
+                      </p>
+                    </div>
 
-        <div className="field">
-          <label className="label">Password</label>
-          <div className="field-body">
-            <div className="field">
-              <p className="control is-expanded">
-                <input
-                  className="input"
-                  type="password"
-                  placeholder="Password"
-                  ref={password}
-                />
-              </p>
-            </div>
-
-            <div className="field">
-              <p className="control is-expanded">
-                <input
-                  className="input"
-                  type="password"
-                  placeholder="Verify Password"
-                  ref={verifyPassword}
-                />
-              </p>
-            </div>
+                    <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                      <p className="block text-xs font-medium text-gray-900">
+                        <input
+                          className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                          type="password"
+                          placeholder="Verify Password"
+                          ref={verifyPassword}
+                        />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <fieldset className="space-y-5">
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      Current Role:
+                    </label>
+                    <div className="">
+                      <input
+                        className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                        placeholder="Where are you working?"
+                        ref={current_role}
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="formSection rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+                    <label className="block text-xs font-medium text-gray-900">
+                      Bio
+                    </label>
+                    <div className="control">
+                      <textarea
+                        className="form-input block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                        placeholder="Tell us about yourself..."
+                        ref={bio}
+                      ></textarea>
+                    </div>
+                  </div>
+                </fieldset>
+                <div className="field is-grouped">
+                  <div className="control">
+                    <button
+                      className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 mb-2 mt-4 w-40"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </section>
           </div>
         </div>
-
-        <div className="field">
-          <label className="label">Current Role:</label>
-          <div className="control">
-            <input
-              className="textarea"
-              placeholder="Where are you working?"
-              ref={current_role}
-            ></input>
-          </div>
-        </div>
-        <div className="field">
-          <label className="label">Bio</label>
-          <div className="control">
-            <textarea
-              className="textarea"
-              placeholder="Tell us about yourself..."
-              ref={bio}
-            ></textarea>
-          </div>
-        </div>
-        <div className="field is-grouped">
-          <div className="control">
-            <button
-              className="button is-link"
-              type="submit"
-            >
-              Submit
-            </button>
-          </div>
-          <div className="control">
-            <Link to="/login" className="button is-link is-light">
-              Cancel
-            </Link>
-          </div>
-        </div>
-      </form>
-    </section>
+      </div>
+      <section className="bg-home w-1/2 flex justify-center">
+        <img className="h-3/4" src={SalyDeskImage}></img>
+      </section>
+    </main>
   );
 }
