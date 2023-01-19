59f3cf784f48e0b973b5b67fd2d3bb582f17136f
Jonathan Woodard <jvw.226@gmail.com>
2023-01-19 09:56:31 -0500

    added comments for admin views

--- src/components/views/AdminViews.js
+++ src/components/views/AdminViews.js
@@ -4,14 +4,14 @@ import { AdminDashboard } from "../admin/AdminDashboard";
 import { ResourceNotFound } from "../errorPages/NotFound";
 import { Authorized } from "./Authorized";
 
+// Admin Views Component handles the routes for an admin user.
+
 export const AdminViews = ({ token }) => {
-
-
   return (
     <Routes>
       <Route path="*" element={<ResourceNotFound />} />
       <Route element={<Authorized token={token} />} />
-      <Route path="/dashboard" element={<AdminDashboard />} />
+      <Route path="/admindashboard" element={<AdminDashboard />} />
     </Routes>
   );
 };
